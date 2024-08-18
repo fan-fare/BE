0931bd9701a86ec7980c9843b0200a801c586087
Joy <munchinthepool64@gmail.com>
2024-08-18 18:32:48 +0900

    initial commit

---
+++ src/main/java/cc/happybday/fanfare/FanfareApplication.java
@@ -0,0 +1,13 @@
+package cc.happybday.fanfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanfareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanfareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/happybday/fanfare/FanfareApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.happybday.fanfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanfareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
