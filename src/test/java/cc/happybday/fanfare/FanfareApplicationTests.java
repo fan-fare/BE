026423a410591338e6943b87ae947fba981c229b
Joy <munchinthepool64@gmail.com>
2024-08-30 04:50:42 +0900

    [CHORE] DB 관련 환경 설정 수정

--- src/test/java/cc/happybday/fanfare/FanfareApplicationTests.java
+++ src/test/java/cc/happybday/fanfare/FanfareApplicationTests.java
@@ -2,8 +2,10 @@ package cc.happybday.fanfare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FanfareApplicationTests {
 
 	@Test
