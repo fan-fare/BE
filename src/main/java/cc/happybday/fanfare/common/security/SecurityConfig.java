9ebddcc9dc1f0c403a56d5293d8880414d962f7c
Joy <munchinthepool64@gmail.com>
2024-09-01 01:24:38 +0900

    [CHORE] 경로별 인가 작업

--- src/main/java/cc/happybday/fanfare/common/security/SecurityConfig.java
+++ src/main/java/cc/happybday/fanfare/common/security/SecurityConfig.java
@@ -46,9 +46,8 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/signup", "/members/exists", "/").permitAll() // 모든 사용자 허용
-                        .anyRequest().authenticated()); // 로그인한 사용자만 허용
-//                        .anyRequest().permitAll()); // 모든 사용자 허용 (임시)
+                        .requestMatchers("DELETE").authenticated()
+                        .anyRequest().permitAll()); // 모든 사용자 허용
 
         http
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
