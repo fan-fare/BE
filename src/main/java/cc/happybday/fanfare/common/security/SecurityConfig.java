2cace9ec733efeaafa2f711358b14415aabd12ec
Joy <munchinthepool64@gmail.com>
2024-09-01 19:29:32 +0900

    [CHORE] 경로별 인가 작업

--- src/main/java/cc/happybday/fanfare/common/security/SecurityConfig.java
+++ src/main/java/cc/happybday/fanfare/common/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -83,7 +84,8 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("DELETE").authenticated()
+                        .requestMatchers(HttpMethod.DELETE).authenticated()
+                        .requestMatchers(HttpMethod.GET, "/message/*").authenticated()
                         .anyRequest().permitAll()); // 모든 사용자 허용
 
         http
