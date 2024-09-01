d0abc5286a49d5acc9b4b9f99a929c36b7271bf0
Joy <munchinthepool64@gmail.com>
2024-09-01 16:53:03 +0900

    [FEAT] CORS 설정

--- src/main/java/cc/happybday/fanfare/common/security/SecurityConfig.java
+++ src/main/java/cc/happybday/fanfare/common/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package cc.happybday.fanfare.common.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -34,6 +40,37 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // cors 설정
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        // 허용할 Origin 설정
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://happybday.cc/"));
+
+                        // 허용할 HTTP 메서드 설정 - 모든 메서드(GET, POST, PUT, DELETE 등) 허용
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+
+                        // 자격 증명(쿠키, Authorization 헤더 등)을 포함한 요청을 허용할지 설정
+                        configuration.setAllowCredentials(true);
+
+                        // 허용할 HTTP 헤더 설정 - 여기서는 모든 헤더를 허용
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+
+                        // 3600초(1시간) 동안 캐시
+                        configuration.setMaxAge(3600L);
+
+                        // 클라이언트에게 노출할 헤더 설정 - Authorization 헤더를 노출하여 클라이언트가 접근할 수 있게 설정
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         http
                 .csrf((auth) -> auth.disable());
