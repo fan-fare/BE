fcf709273a14d81cbffa073422d747bb0f323199
Joy <munchinthepool64@gmail.com>
2024-09-02 12:55:30 +0900

    [FEAT] 7일전 날짜에 생일인 회원에 해당하는 Member, Message 엔티티 삭제

--- src/main/java/cc/happybday/fanfare/repository/MessageRepository.java
+++ src/main/java/cc/happybday/fanfare/repository/MessageRepository.java
@@ -1,5 +1,6 @@
 package cc.happybday.fanfare.repository;
 
+import cc.happybday.fanfare.domain.Member;
 import cc.happybday.fanfare.domain.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,5 +36,8 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     Optional<Long> findMessagePosition(@Param("memberId") Long memberId, @Param("messageId") Long messageId);
 
     Page<Message> findAllByMember_IdOrderByCreatedAtAsc(Long memberId, Pageable pageable);
+
+    void deleteAllByMember(Member member);
+
 
 }
