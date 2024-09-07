195eb985de843a89f469e13b0256d97b1c553562
Joy <munchinthepool64@gmail.com>
2024-09-07 16:56:28 +0900

    [FEAT] token 만료시 401 반환

--- src/main/java/cc/happybday/fanfare/common/response/ErrorResponseCode.java
+++ src/main/java/cc/happybday/fanfare/common/response/ErrorResponseCode.java
@@ -16,11 +16,13 @@ public enum ErrorResponseCode {
     MEMBER_NOT_FOUND("F05", "회원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
     MEMBER_ROLE_NOT_FOUND("F06", "회원의 ROLE 값을 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
     UNAUTHORIZED_MEMBER("F07", "인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED.value()),
-    FORBIDDEN_ACCESS("F07", "이 리소스에 접근할 권한이 없습니다.", HttpStatus.FORBIDDEN.value()),
+    FORBIDDEN_ACCESS("F08", "이 리소스에 접근할 권한이 없습니다.", HttpStatus.FORBIDDEN.value()),
 
-    // Message
-    MESSAGE_NOT_FOUND("F08", "메세지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value());
+    // Message,
+    MESSAGE_NOT_FOUND("F09", "메세지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
 
+    // token
+    TOKEN_EXPIRED("F10", "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED.value());
 
     private final String code;
     private final String message;
